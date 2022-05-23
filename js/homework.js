975c381c03d310a07c8ef157f82df6144c219ff1
franciscomtz0719 <franciscomtz0719@gmail.com>
2022-05-22 20:48:52 -0500

    Class four notes and homework added

--- js/homework.js
+++ js/homework.js
@@ -1,91 +1,171 @@
+// Ejercicios
+
+/**
+ * Ejercicio 1
+ * Escribir un programa de arroje la suma de los multiplos de 3, 5 o 7 que hay entre 1 y 100
+ * Resolver con For, While y do While
+ */
+
+// let iteratorEnd = 101
+// let mutiplyRes1 = 0
+// let mutiplyRes2 = 0
+// let mutiplyRes3 = 0
+// let iteratorStart = 3
+
+//CICLO FOR
+// for (let i = 3; i <iteratorEnd; i++) {
+
+//     mutiplyRes1 = i % 3
+//     mutiplyRes2 = i % 5
+//     mutiplyRes3 = i % 7
+
+//     if (mutiplyRes1 === 0 ){
+//         console.log(i)
+//     }else if (mutiplyRes2 === 0){
+//         console.log(i)
+//     }else if (mutiplyRes3 === 0){
+//         console.log(i)
+//     }
+
+// }
+
+// CICLO WHILE
+// while (iteratorEnd >= iteratorStart){
+//         mutiplyRes1 = iteratorStart % 3
+//         mutiplyRes2 = iteratorStart % 5
+//         mutiplyRes3 = iteratorStart % 7
+
+//         if (mutiplyRes1 === 0 ){
+//             console.log(iteratorStart)
+//         }else if (mutiplyRes2 === 0){
+//             console.log(iteratorStart)
+//         }else if (mutiplyRes3 === 0){
+//             console.log(iteratorStart)
+//         }
+
+//         iteratorStart++
+// }
+
+// CICLO DO WHILE
+
+// do{
+//             mutiplyRes1 = iteratorStart % 3
+//             mutiplyRes2 = iteratorStart % 5
+//             mutiplyRes3 = iteratorStart % 7
+
+//             if (mutiplyRes1 === 0 ){
+//                 console.log(iteratorStart)
+//             }else if (mutiplyRes2 === 0){
+//                 console.log(iteratorStart)
+//             }else if (mutiplyRes3 === 0){
+//                 console.log(iteratorStart)
+//             }
+
+//             iteratorStart++
+// } while (iteratorEnd >= iteratorStart)
 
 
 
 /**
  * Ejercicio 2
- * Pedir al usuario usuario un numero entre 1 y 10
- * Imprimir la tabla de multiplicar de ese número
- * p.ej. 3
- * 3 x 1 = 3
- * 3 x 2 = 6
- * ....
- * 3 x 10 = 30
+ * Pedir el usuario 1 numero entre 1 y 100
+ * sumar todos los numeros entre el 1 y este numero
+ * Mandar un alert con el  total
+ * Resolver con For, While y do While
+ * p.ej. 5
+ * ->  15
  */
 
-// let multiplier = parseInt(prompt('Ingrese un la tabla que desea consultar del 1-10'))
-
-// if(multiplier <= 10){
-//     for (i = 1; i <=10; i++) {
-//         console.log(` ${multiplier} x ${i} = ${multiplier*i}`)  
-// }
-// } else {
-//     console.log('Número fuera de rango')
+// let endPoint = parseInt(prompt('Ingrese un número entre 1 y 100'))
+// let suma = 0
+// let iterator = 0
+//CICLO FOR
+// for (let i = 0; i <= endPoint; i++) {
+//     suma= suma + i
 // }
 
+//CICLO While
+// while (iterator <= endPoint) {
+
+//     suma = suma + iterator
+//     iterator++
+// }
+
+//CICLO DO WHILE20
+
+// do {
+//     suma = suma + iterator
+//     iterator++
+// } while (iterator <= endPoint)
+
+
+// window.alert(`El resultado de la suma es ${suma}`)
 
 /**
  * Ejercicio 3
- * Pedir al usuario una oracion
- * Imprimir un string con todas las vocales
- * Imprimir un string con todas las consonantes
- * p.ej. "Hola mundo"
- * Consonantes -> hlmnd
- * Vocales -> oauo
+ * pedir una oracion al usuario
+ * Contar las letras "a" y "e" y espacios,
+ * Resolver con For, While y do While
+ * 
+ * p.ej. 'hola kodErs'
+ * ->  A -> 1 
+ * ->  E -> 1
+ * ->  Espacios -> 1 
  */
 
-//   let phrase = prompt('ingrese una oración')
-//   let fin = phrase.length -1
+
+let str = prompt('Ingrese una oración', 'Hola E')
+let containerA = 0
+let acomulator = str.length
+let containerE = 0
+let counter = 0
+let strMin = str.toLowerCase()
+
+// for (let i = 0; i < acomulator; i++) {   
+//     if( strMin[i]==='a'){
+//         containerA += 1 
+//     }else if( strMin[i]==='e'){
+//         containerE += 1
+//     }else{
+//         console.error ('no contiene letras A ni E')
+//     }
+// }
+
+// while(counter<acomulator){
+//         if( strMin[counter]==='a'){
+//         containerA += 1 
+//     }else if( strMin[counter]==='e'){
+//         containerE += 1
+//     }
+//     counter++
+// }
+
+do {
+    if( strMin[counter]==='a'){
+        containerA += 1 
+    }else if( strMin[counter]==='e'){
+        containerE += 1
+    }
+    counter++
+} while(counter<acomulator)
+
+
+console.log(`La oración "${str}" contiene ${containerA} Letras A`)
+console.log(`La oración "${str}" contiene ${containerE} Letras E`)
 
 
 
 /**
- * 
  * Ejercicio 4
- * Pedir el usuario un numero entre 10 y 100
- * Imprimir todos los numeros pares que existen entre 1 y ese numero
- * p.ej. 12 -> 2,4,6,8,10,12
+ * Estudiar acerca del tema Funciones
+ * ¿Qué son?
+ * Parametros y argumentos de una funcion
+ * Parametros de una funcion
+ * Parametros opcionales
+ * Parametros por defecto (default)
+ * 
+ * Referencias
+ * https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Functions
+ * https://www.programiz.com/javascript/function
  * 
  */
-
-//    let even = parseInt(prompt('ingrese un numero entre 10 y 100'))
-//    let k = 2
-//    let evenConcatenate = ""
- 
-//   if (even >= 10 && even <= 100) {
-//       while (k <= even) {
-               
-//         evenConcatenate += [k,'']
-//         k += 2
-
-
-//   }
-//   console.log(evenConcatenate)
-// } else{
-//     console.log('Numero fuera de rango')
-// }
-   
-
-//palabra en reversa
-
-const nameUser = prompt ('escribe un a plabra')
-let strReversed = ''
-
-for(let i = nameUser.length - 1; i >= 0; i--){
-    strReversed += nameUser[i]
-    console.log('i')
-}
-console.log(strReversed)
-
-
-
-  
-
-    
-
-
-
-
-
-
-
-
