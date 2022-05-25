2ea52e920b0e47e2879d2bae587cf5caf88f9e1f
franciscomtz0719 <franciscomtz0719@gmail.com>
2022-05-24 19:43:09 -0500

    Class four notes and homework

--- js/homework.js
+++ js/homework.js
@@ -1,171 +1,118 @@
-// Ejercicios
-
-/**
- * Ejercicio 1
- * Escribir un programa de arroje la suma de los multiplos de 3, 5 o 7 que hay entre 1 y 100
- * Resolver con For, While y do While
- */
-
-// let iteratorEnd = 101
-// let mutiplyRes1 = 0
-// let mutiplyRes2 = 0
-// let mutiplyRes3 = 0
-// let iteratorStart = 3
-
-//CICLO FOR
-// for (let i = 3; i <iteratorEnd; i++) {
-
-//     mutiplyRes1 = i % 3
-//     mutiplyRes2 = i % 5
-//     mutiplyRes3 = i % 7
-
-//     if (mutiplyRes1 === 0 ){
-//         console.log(i)
-//     }else if (mutiplyRes2 === 0){
-//         console.log(i)
-//     }else if (mutiplyRes3 === 0){
-//         console.log(i)
-//     }
-
-// }
-
-// CICLO WHILE
-// while (iteratorEnd >= iteratorStart){
-//         mutiplyRes1 = iteratorStart % 3
-//         mutiplyRes2 = iteratorStart % 5
-//         mutiplyRes3 = iteratorStart % 7
-
-//         if (mutiplyRes1 === 0 ){
-//             console.log(iteratorStart)
-//         }else if (mutiplyRes2 === 0){
-//             console.log(iteratorStart)
-//         }else if (mutiplyRes3 === 0){
-//             console.log(iteratorStart)
-//         }
-
-//         iteratorStart++
-// }
-
-// CICLO DO WHILE
-
-// do{
-//             mutiplyRes1 = iteratorStart % 3
-//             mutiplyRes2 = iteratorStart % 5
-//             mutiplyRes3 = iteratorStart % 7
-
-//             if (mutiplyRes1 === 0 ){
-//                 console.log(iteratorStart)
-//             }else if (mutiplyRes2 === 0){
-//                 console.log(iteratorStart)
-//             }else if (mutiplyRes3 === 0){
-//                 console.log(iteratorStart)
-//             }
-
-//             iteratorStart++
-// } while (iteratorEnd >= iteratorStart)
-
 
 
 /**
- * Ejercicio 2
- * Pedir el usuario 1 numero entre 1 y 100
- * sumar todos los numeros entre el 1 y este numero
- * Mandar un alert con el  total
- * Resolver con For, While y do While
- * p.ej. 5
- * ->  15
- */
-
-// let endPoint = parseInt(prompt('Ingrese un número entre 1 y 100'))
-// let suma = 0
-// let iterator = 0
-//CICLO FOR
-// for (let i = 0; i <= endPoint; i++) {
-//     suma= suma + i
-// }
-
-//CICLO While
-// while (iterator <= endPoint) {
-
-//     suma = suma + iterator
-//     iterator++
-// }
-
-//CICLO DO WHILE20
-
-// do {
-//     suma = suma + iterator
-//     iterator++
-// } while (iterator <= endPoint)
-
-
-// window.alert(`El resultado de la suma es ${suma}`)
-
-/**
- * Ejercicio 3
- * pedir una oracion al usuario
- * Contar las letras "a" y "e" y espacios,
- * Resolver con For, While y do While
- * 
- * p.ej. 'hola kodErs'
- * ->  A -> 1 
- * ->  E -> 1
- * ->  Espacios -> 1 
+ * Funcion que pida una palabra al usuario
+ * Invierta la palabra
+ * y la retorne en un alert
+ * reversedString('hola') -> 'aloh' 
  */
 
 
-let str = prompt('Ingrese una oración', 'Hola E')
-let containerA = 0
-let acomulator = str.length
-let containerE = 0
-let counter = 0
-let strMin = str.toLowerCase()
+function reversedString(){
 
-// for (let i = 0; i < acomulator; i++) {   
-//     if( strMin[i]==='a'){
-//         containerA += 1 
-//     }else if( strMin[i]==='e'){
-//         containerE += 1
-//     }else{
-//         console.error ('no contiene letras A ni E')
-//     }
-// }
+    let str = prompt('Ingrese una oración')
+    let strLenght = str.length
+    let strReversed = ''
 
-// while(counter<acomulator){
-//         if( strMin[counter]==='a'){
-//         containerA += 1 
-//     }else if( strMin[counter]==='e'){
-//         containerE += 1
-//     }
-//     counter++
-// }
-
-do {
-    if( strMin[counter]==='a'){
-        containerA += 1 
-    }else if( strMin[counter]==='e'){
-        containerE += 1
+    for (i = strLenght-1; i >= 0; i--) {
+        strReversed += str[i]
     }
-    counter++
-} while(counter<acomulator)
+    window.alert(`reversedString ('${str}') --> ${strReversed}`)
+}
 
 
-console.log(`La oración "${str}" contiene ${containerA} Letras A`)
-console.log(`La oración "${str}" contiene ${containerE} Letras E`)
 
 
 
 /**
- * Ejercicio 4
- * Estudiar acerca del tema Funciones
- * ¿Qué son?
- * Parametros y argumentos de una funcion
- * Parametros de una funcion
- * Parametros opcionales
- * Parametros por defecto (default)
- * 
- * Referencias
- * https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Functions
- * https://www.programiz.com/javascript/function
- * 
+ * Funcion que pida 3 numeros como parametro
+ * 2 numeros obligatorios y 1 opcional con valor de 3
+ * Arroje la suma de esos 3 numeros
+ * Posibles resultados
+ * -> addThreeNumbers(3,4,5) -> 12
+ * -> addThreeNumbers(3,4) -> 10
+ * -> addThreeNumbers(3) -> 'Faltan datos'
+ * -> addThreeNumbers() -> 'Faltan datos'
  */
+
+// function addThreeNumbers(a, b, c=3){
+//     if ((isNaN(a,b)) || 
+//         (isNaN(c,b)) ||
+//         (isNaN(a,b,c))){
+//         return 'Faltan datos'
+//     }
+//     else{
+//         return a+b+c
+//     }
+
+// }
+
+// let p1 = parseInt(prompt('ingrese un numero1')) 
+// let p2 = parseInt(prompt('ingrese un numero2')) 
+// let p3 = parseInt(prompt('ingrese un numero3'))
+// let sumaTotal = addThreeNumbers (p1, p2, p3)
+// console.log(sumaTotal)
+
+/**
+ * Funcion que pida al usuario los grados Centigrados en su localidad
+ * Convertirlos a grados fahrenheit
+ * Arrojar un alert con el resultado
+ * -> convertTemp(25) -> La temperatura es de 77 ºF
+ * -> convertTemp() -> 'Faltan datos'
+ */
+
+function convertTemp(){
+
+    let gradosC = parseInt(prompt('Ingrese la temperatura en °C', 18))
+    let conversion = 0
+
+    if (isNaN(gradosC)){
+        window.alert('faltan datos')
+    }else{
+        conversion =  gradosC * 9/5 + 32
+        window.alert(`La temperatura en °F es ${conversion}`)
+    }    
+
+
+}
+
+
+/**
+ * Funcion que imprima las tablas del 1 al 10
+ */
+
+function multiplication () {
+    let result = 0
+
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            result = i * j
+            console.log(`${i} x ${j} = ${result}`)
+        }
+ 
+    }
+    
+}
+
+
+/**
+ * Funcion que pida al usuario un numero (N) entre 1 y 100 
+ * Mandar un alert con suma de 1 a N
+ * Valor por defecto 3
+ * addNumberLimit() -> 6
+ * addNumberLimit(4) -> 10
+ * addNumberLimit(5) -> 15
+ */
+
+function addNumberLimit (a=3){
+    let numberLimit = parseInt(prompt('ingrese un numero entre 1 y 100'))
+    let sumContainer = 0
+
+    for (let a=0; a<= numberLimit; a++){
+
+        sumContainer = sumContainer + a
+    }
+
+    console.log (sumContainer)
+}
+
